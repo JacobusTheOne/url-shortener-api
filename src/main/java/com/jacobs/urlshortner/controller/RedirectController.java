68e0f1f7a51c039bed21b0bf10ccba17b61b2f84
JacobusTheOne <jacobus.jacobs1@yahoo.com>
2026-01-16 18:13:58 -0300

    Removed unneccesary import: import org.springframework.http.ResponseEntity;

--- src/main/java/com/jacobs/urlshortner/controller/RedirectController.java
+++ src/main/java/com/jacobs/urlshortner/controller/RedirectController.java
@@ -6,7 +6,6 @@ import com.jacobs.urlshortner.service.UrlShortenerService;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;

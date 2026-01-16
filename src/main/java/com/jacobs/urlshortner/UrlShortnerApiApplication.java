dfa962dcdf3cff4bdb0eac9fd0a9018384148233
JacobusTheOne <jacobus.jacobs1@yahoo.com>
2026-01-16 13:20:54 -0300

    Configure H2 in-memory database for local development

---
+++ src/main/java/com/jacobs/urlshortner/UrlShortnerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jacobs.urlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerApiApplication.class, args);
+	}
+
+}
